f248c7be77973faf7451e3b1482056e87ef6478b
Abhishekkkk-15 <mrabhi748@gmail.com>
2026-04-16 12:11:39 +0530

    feat: load config

--- main.go
+++ main.go
@@ -1,5 +1,11 @@
 package main
 
+import (
+	"encoding/json"
+	"os"
+	"path/filepath"
+)
+
 type Config struct {
 	GroqAPIKey     string `json:"groq_api_key"`
 	Model          string `json:"model"`
@@ -29,4 +35,51 @@ func DefaultConfig() Config {
 		MaxBodyLineLen: 80,
 		StrictMode:     false,
 	}
+}
+
+func LoadConfig() (*Config, error) {
+	configPath, err := getConfigPath()
+	if err != nil {
+		return nil, err
+	}
+	data, err := os.ReadFile(configPath)
+	if err != nil {
+		if os.IsNotExist(err) {
+			defaultConfig := DefaultConfig()
+			saveErr := SaveConfig(&defaultConfig)
+			if saveErr != nil {
+				return nil, saveErr
+			}
+			return &defaultConfig, nil
+		}
+		return nil, err
+	}
+	var config Config
+	err = json.Unmarshal(data, &config)
+	return &config, err
+}
+
+func SaveConfig(config *Config) error {
+	configPath, err := getConfigPath()
+	if err != nil {
+		return err
+	}
+	data, err := json.MarshalIndent(config, "", " ")
+	if err != nil {
+		return err
+	}
+	return os.WriteFile(configPath, data, 0600)
+	return nil
+}
+
+func getConfigPath() (string, error) {
+	home, err := os.UserHomeDir()
+	if err != nil {
+		return "", err
+	}
+	configDir := filepath.Join(home, ".commit-assistant")
+	if err := os.MkdirAll(configDir, 0700); err != nil {
+		return "", err
+	}
+	return filepath.Join(configDir, "config.json"), nil
 }
